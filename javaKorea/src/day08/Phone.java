34ae6a5a50cae25fd8e6b718aaa44d3fa59d47f7
pyj3717 <pyj3717@naver.com>
2022-11-20 12:35:33 +0900

    자바7일차

---
+++ javaKorea/src/day08/Phone.java
@@ -0,0 +1,38 @@
+package day08;
+
+public class Phone {
+	public static void main(String[] args) {
+		PhoneImpl pi = new PhoneImpl();
+		pi.bigdata();
+		pi.Net();
+		pi.Msg();
+		pi.Call();
+		pi.speed();
+	}
+}
+
+// Interface(선언만) -> InterfaceImpl(구현까지)
+// Impl : 구현부
+class PhoneImpl extends AbstractClass1{
+	
+	@Override
+	public void Call() {
+		System.out.println("전화를 합니다");
+	}
+	
+	@Override
+	public void Msg() {
+		System.out.println("문자를 보냅니다");
+	}
+	
+	@Override
+	public void Net() {
+		System.out.println("인터넷을 합니다");
+	}
+	
+	@Override
+	public void speed() {
+		System.out.println("LTE 빠름");
+	}
+	
+}
---
+++ javaKorea/src/day08/View.java
@@ -0,0 +1,32 @@
+package day08;
+
+// 출력하는 부분
+public class View {
+	public static void main(String[] args) {
+		Model model = new Model(3000,"아메리카노");		// Model 클래스에 있는 변술르 추가한 셈
+		// model 변수에 3000,"아메리카노" 저장
+		
+		Controller con = new Controller(model);
+		
+		// syso
+		System.out.println(con.sell()+"을(를) 판매합니다.");
+		System.out.println(con.info().getMenu()+"을(를) 가격은 "+con.info().getPrice()+"원 입니다.");
+	}
+}			// mvc패턴(Model-View-Controller 코딩방식)
+
+// 흐름(로직)을 제어하는 부분
+class Controller {
+	int num;
+	Model model = new Model();
+	public Controller(Model model/* int price, String menu */) {
+		this.model.setPrice(model.getPrice());
+		this.model.setMenu(model.getMenu());
+	}
+	
+	public String sell() {
+		return model.getMenu();
+	}
+	public Model info() {
+		return model;
+	}
+}
